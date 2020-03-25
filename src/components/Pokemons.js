9410c49b17ec978a7fff945be045d6dbfc8baf05
Gnorbi951 <gnorbi951@gmail.com>
2020-03-25 16:48:37 +0100

    Pokémons page keep the navbar

--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 export default function Pokemons() {
   return (
     <div>
+      <Navbar />
       <h1>This will be the pokemon page</h1>
     </div>
   );
