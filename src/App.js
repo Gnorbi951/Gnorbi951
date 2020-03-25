9c7d123162dd9c363527274c2062098cb42e50c7
Gnorbi951 <gnorbi951@gmail.com>
2020-03-25 15:11:53 +0100

    change function to class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Test</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
