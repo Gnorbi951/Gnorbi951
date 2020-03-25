833807d5dac8e56b4b1c695391af4dcfcfc219b5
Gnorbi951 <gnorbi951@gmail.com>
2020-03-25 15:33:16 +0100

    Add navbar picture

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
 import React from "react";
+import "./navbar_style.css";
 
 export default function Navbar() {
   return (
-    <div>
+    <div class="navbar">
+      <img
+        alt="navbar_picture"
+        src="https://lh3.googleusercontent.com/proxy/jC2wXWqrTjVxTMdgqEAc_gV0YCJ-xm1kuvYHGbEk23Gz6U_Xz8Glm2TPwvnYCAORl8RE1LkCHv-gS2rp3A1qzYFixcq6NpEQrqiHN0d67SlnJwRi1EKhSq0e2XB_XJfAmy8aPE86zK_pog"
+      ></img>
       <h2>This will be my navbar</h2>
     </div>
   );
