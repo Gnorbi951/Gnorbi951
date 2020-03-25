4db7de41644f7f3e7efb9d523a8618b113f9b92e
Gnorbi951 <gnorbi951@gmail.com>
2020-03-25 20:16:05 +0100

    finally i can render the page

--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -17,7 +17,11 @@ class Pokemons extends Component {
     return (
       <div>
         <Navbar />
-        <h1>This will be the pokemon page</h1>
+        <div>
+          {this.state.pokemon.map(pok => (
+            <li>{pok.name}</li>
+          ))}
+        </div>
       </div>
     );
   }
