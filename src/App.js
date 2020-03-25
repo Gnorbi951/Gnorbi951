17dfd70fec0dd04b52db8c909a389f0a1ffce07d
Gnorbi951 <gnorbi951@gmail.com>
2020-03-25 15:51:37 +0100

    Add Router and browserRoter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Navbar />
-        <h1>Test</h1>
-      </div>
+      <BrowserRouter>
+        <Route
+          path="/"
+          render={() => (
+            <React.Fragment>
+              <div className="container">
+                <Navbar />
+                <h1>Test</h1>
+              </div>
+            </React.Fragment>
+          )}
+        />
+      </BrowserRouter>
     );
   }
 }
