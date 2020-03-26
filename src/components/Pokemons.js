fff12ef0b00e5831de73476ced7c31688cdb96ac
Gnorbi951 <gnorbi951@gmail.com>
2020-03-26 14:36:41 +0100

    didn't save on prevoius commit

--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -16,16 +16,14 @@ class Pokemons extends Component {
       this.setState({ prev: resp.data.previous });
       const pokemonData = resp.data.results;
       let pokemons = [];
-      pokemonData.map(
-        ...data => {
-          let pokemon = { name: data.name, picture: "" };
-          axios.get(data.url).then(url => {
-            pokemon.picture = url.data.sprites.front_default;
-            pokemons.push(pokemon);
-            this.setState({ isLoaded: true });
-          });
-        }
-      );
+      pokemonData.map(data => {
+        let pokemon = { name: data.name, picture: "" };
+        axios.get(data.url).then(url => {
+          pokemon.picture = url.data.sprites.front_default;
+          pokemons.push(pokemon);
+          this.setState({ isLoaded: true });
+        });
+      });
       this.setState({ pokemons: pokemons });
     });
   }
