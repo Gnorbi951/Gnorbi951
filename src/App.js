8d8c51b633a3e8612f0c56a4866dd8ed00b12bbe
Gnorbi951 <gnorbi951@gmail.com>
2020-03-28 19:15:03 +0100

    rewrite app.js into functional component

--- src/App.js
+++ src/App.js
@@ -6,30 +6,28 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Pokemon from "./components/Pokemon";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <React.Fragment>
-              <div className="container">
-                <Navbar />
-                <h1>Test</h1>
-              </div>
-            </React.Fragment>
-          )}
-        />
-        <Switch>
-          <Route path="/pokemons" component={Pokemons}></Route>
-          <Route path="/types" component={Types}></Route>
-          <Route path="/pokemon/:id" component={Pokemon}></Route>
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <React.Fragment>
+            <div className="container">
+              <Navbar />
+              <h1>Test</h1>
+            </div>
+          </React.Fragment>
+        )}
+      />
+      <Switch>
+        <Route path="/pokemons" component={Pokemons}></Route>
+        <Route path="/types" component={Types}></Route>
+        <Route path="/pokemon/:id" component={Pokemon}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
