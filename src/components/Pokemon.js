93a16052b188aa855d58b5c1f355e27cea21af86
Gnorbi951 <gnorbi951@gmail.com>
2020-03-29 16:33:22 +0200

    change pokemon to functional component

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,44 +1,41 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import axios from "axios";
 
-export default class Pokemon extends Component {
-  state = {
-    name: null,
-    height: null,
-    abilities: [],
-    image: null
-  };
+const Pokemon = props => {
+  const [name, setName] = useState({ name: "" });
+  const [height, setHeight] = useState({ height: 0 });
+  const [abilities, setAbilities] = useState({ abilities: [] });
+  const [image, setImage] = useState({ image: "" });
 
-  componentDidMount() {
-    const { id } = this.props.match.params;
+  useEffect(() => {
+    const { id } = props.match.params;
     axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`).then(resp => {
       const data = resp.data;
-      console.log(data);
       const pokemonName = data.name;
       const nameCapitalized =
         pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
-      this.setState({ name: nameCapitalized });
-      this.setState({ height: data.height });
-      this.setState({ abilities: data.abilities });
-      this.setState({ image: data.sprites.front_default });
+      setName({ name: nameCapitalized });
+      setHeight({ height: data.height });
+      setAbilities({ abilities: data.abilities });
+      setImage({ image: data.sprites.front_default });
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <div class="card_container">
-          <h1 id="poke_name"> {this.state.name}</h1>
-          <h2 id="poke_height">Height: {this.state.height}"</h2>
-          <h3 id="pokebilities">Abilities:</h3>
-          {this.state.abilities.map(array => (
-            <li class="poke_list_item">{array.ability.name}</li>
-          ))}
-          <img alt="pokemon_image" id="poke_img" src={this.state.image}></img>
-        </div>
+  return (
+    <div>
+      <Navbar />
+      <div class="card_container">
+        <h1 id="poke_name"> {name.name}</h1>
+        <h2 id="poke_height">Height: {height.height}"</h2>
+        <h3 id="pokebilities">Abilities:</h3>
+        {abilities.abilities.map(array => (
+          <li class="poke_list_item">{array.ability.name}</li>
+        ))}
+        <img alt="pokemon_image" id="poke_img" src={image.image}></img>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Pokemon;
